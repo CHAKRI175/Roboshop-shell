a086409e178760367c06d59024e7e04c2c77b460
chakradhar seeramsetty <chakrichakradhar148@gmail.com>
2026-02-04 11:53:04 +0530

    added redis.sh

---
+++ redis.sh
@@ -0,0 +1,49 @@
+owner=($(id -u))
+logs_dir="/var/log/RoboShop-shell" 
+log_file="$logs_dir/$0.log"
+R="\e[31m"
+G="\e[32m"      
+Y="\e[33m"
+B="\e[34m"
+NC="\e[0m"
+Path=$PWD
+if [ $owner -ne 0 ]; then
+    echo -e "$R Please run this script as root. $NC" | tee -a $log_file
+    exit 1
+fi          
+ 
+mkdir -p $logs_dir
+validate(){ 
+
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 $R installation failed. $NC" | tee -a $log_file
+        exit 1
+    else
+        echo -e "$2 $G installation successful. $NC" | tee -a $log_file
+    fi
+}
+
+dnf module disable redis -y
+validate $? "redis module disable"
+
+dnf module enable redis:7 -y
+validate $? "redis module enable"
+
+dnf install redis -y | &>>$log_file
+validate $? "redis-server"
+
+echo -e "$G Redis installation completed successfully. $NC" | tee -a $log_file
+
+sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' /etc/redis/redis.conf
+
+systemctl enable redis
+validate $? "redis service enable and start"
+
+systemctl status redis | &>>$log_file
+validate $? "redis service status check"
+
+systemctl start redis
+validate $? "redis service start"
+
+
+
