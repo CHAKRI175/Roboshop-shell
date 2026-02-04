ba10a3bfcbcd7454354a23c8eb58d98a8af752af
chakradhar seeramsetty <chakrichakradhar148@gmail.com>
2026-02-04 23:23:18 +0530

    updated logvariable.

--- shipping.sh
+++ shipping.sh
@@ -75,7 +75,7 @@ validate $? "mysql client install"
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
 if [ $? -ne 0 ]; then
 
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql $>>$log_file
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$log_file
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$log_file
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$log_file
     validate $? "Loaded data into MySQL"
